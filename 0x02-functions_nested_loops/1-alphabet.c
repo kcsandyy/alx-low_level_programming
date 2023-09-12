47c27e33d67376d28eb0cbda8cd7953a7e5f657d
kcsandyy <kemboisandra20@gmail.com>
2023-09-12 14:42:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet -  prints the alphabet, in lowercase, followed by a new line
+ * Return: always void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		_putchar(i);
+	_putchar('\n');
+}
