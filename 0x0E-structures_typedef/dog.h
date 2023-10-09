0a0651ba77b78f54763ac7f7518dfbce05fcf3a8
kcsandyy <kemboisandra20@gmail.com>
2023-10-09 15:17:06 -0700

    Structs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_DOG
+#define STRUCT_DOG
+/**
+ * struct dog - Basic puppy description
+ * @name: The puppy's name
+ * @age: The puppy's age
+ * @owner: The puppy's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
