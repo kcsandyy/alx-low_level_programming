33d2918a19373eeadfe6106904eb6fac18883ecd
kcsandyy <kemboisandra20@gmail.com>
2023-10-11 14:33:37 -0700

    checks operand

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,11 +21,13 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 5)
+	while (ops[i].op != NULL)
 	{
-		if (*s == *(ops[i].op))
+		if (*(ops[i].op) == *s && *(s + 1) == '\0')
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
-	return (ops[i].f);
+	return (NULL);
 }
