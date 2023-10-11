25080a9f4113e1b43f5211055be5414a4f43c03a
kcsandyy <kemboisandra20@gmail.com>
2023-10-11 13:45:43 -0700

    array function callback

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size = 0)
-		return;
-
 	for (i = 0; i < size; i++)
 	{
-		if (array != NULL || action != NULL)
+		if (array != NULL || action != NULL || size > 0)
 		{
 			action(array[i]);
 		}
