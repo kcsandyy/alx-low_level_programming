46e2d0d697a5f7ce1781a880b8a1d3d2c4dc71b3
kcsandyy <kemboisandra20@gmail.com>
2023-10-11 13:38:49 -0700

    array function callback

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function
+ * given as a parameter on each element of an array
+ * @array: The array pointer
+ * @size: Size of the array
+ * @action: Pointer to the function you need to use
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array != NULL || action != NULL)
+		{
+			action(array[i]);
+		}
+	}
+}
