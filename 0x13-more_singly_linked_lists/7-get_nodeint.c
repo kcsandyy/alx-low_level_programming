a41baa1407c7537f1c12f887e13c54a6f256738b
kcsandyy <kemboisandra20@gmail.com>
2023-10-25 03:36:49 -0700

    Handling indexing of nodes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (node_count == index)
-		       return (head);
+			return (head);
 
 		node_count++;
 		head = head->next;
