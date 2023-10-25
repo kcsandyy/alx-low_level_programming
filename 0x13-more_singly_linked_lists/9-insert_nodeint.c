1d772ff69a087a444878d14366cd04ecd0586758
kcsandyy <kemboisandra20@gmail.com>
2023-10-25 05:45:14 -0700

    Handles insertion of a node in a list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,14 +20,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	new_node->n = n;
 
+	node = *head;
+
 	if (idx == 0)
 	{
-		new_node = *head;
-		*head = new_node;
+		new_node = node;
+		node = new_node;
+		new_node->next = node->next;
+		node->next = new_node;
 		return (new_node);
 	}
 
-	node = *head;
 	while (node != NULL)
 	{
 		if (node_count == idx)
