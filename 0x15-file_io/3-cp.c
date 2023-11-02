fca0df705ca10f5fbbbc6dba837b98fa0169225d
kcsandyy <kemboisandra20@gmail.com>
2023-11-02 05:06:56 -0700

    copying files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,88 +1,68 @@
-#include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
+#include "main.h"
+#include <fcntl.h>
 #include <unistd.h>
 
-#define Buffer_size 1024
-
-void handles_error(int code, char *message, char *file);
-char *greater(char *file1, char *file2);
+#define size 1024
 
 /**
- * handles_error - Handles errors that arise
+ * handles_errors - Handles errors
  * @code: The exit code
  * @message: The error message
- * @file: The file that resulted in the error
+ * @file: The file generating the error
  * Return: Void
  */
-
-void handles_error(int code, char *message, char *file)
+void handles_error(int code, const char *message, const char *file)
 {
 	dprintf(code, "Error: %s %s", message, file);
-	_Exit(code);
+	exit(code);
 }
 
-/**
- * greater - checks a file with the higher value
- * @file1: The first file
- * @file2: The other file
- * Return: The greater file with the max value
- */
-
-char *greater(char *file1, char *file2)
+void cp(const char *file1 ,const char *file2)
 {
-	return (file1 > file2 ? file1 : file2);
-}
-
-/**
- * main - copies the content of a file to another file
- * @argc: Number of arguments
- * @argv: The arguments
- * Return: 0 (Success)
- */
-
-int main(int argc, char *argv[])
-{
-	char buffer[Buffer_size], *file1, *file2;
+	char buffer[size];
 	int file_from, file_to;
 	size_t read_bytes, written_bytes;
 
+	file_from = open(file1, O_RDONLY);
+	if (file_from == -1)
+		handles_error(98, "can't read from file\n", file1);
+	file_to = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (file_to == -1)
+		handles_error(99, "can't write to\n", file2);
+
+	while ((read_bytes = read(file_from, buffer, sizeof(buffer))) > 0)
+	{
+		if (read_bytes == (size_t)(-1))
+			handles_error(98, "can't read from file\n", file1);
+		written_bytes = write(file_to, buffer, read_bytes);
+		if (written_bytes == (size_t)(-1))
+			handles_error(99, "can't write to\n", file2);
+	}
+
+	if (close(file_from) == -1)
+		handles_error(100, "can't close fd\n", file1);
+	if (close(file_to) == -1)
+		handles_error(100, "can't close fd\n", file2);
+}
+
+/**
+ * main - Copies files
+ * @argc: The number of arguments
+ * @argv: The arguments
+ * Return: 0 if successful
+ */
+
+int main(int argc, const char *argv[])
+{
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		_Exit(97);
+		exit(97);
 	}
 
-	file1 = argv[1];
-	file_from = open(file1, O_RDONLY);
-	if (file_from == -1)
-	{
-		handles_error(98, "can't read from file", file1);
-	}
+	cp(argv[1], argv[2]);
 
-	file2 = argv[2];
-	file_to = open(file2, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file_to == -1)
-	{
-		handles_error(99, "can't write to", file2);
-	}
-	while ((read_bytes = read(file_from, buffer, Buffer_size)) > 0)
-	{
-		if (read_bytes == (size_t)(-1))
-		{
-			handles_error(98, "can't read from file", file1);
-		}
-		written_bytes = write(file_to, buffer, read_bytes);
-		if (written_bytes != read_bytes)
-		{
-			handles_error(99, "can't write to", file2);
-		}
-	}
-	if (close(file_from) == -1 || close(file_to) == -1)
-	{
-		handles_error(100, "can't close fd", greater(file1, file2));
-	}
 	return (0);
 }
